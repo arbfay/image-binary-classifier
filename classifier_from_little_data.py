972a883a0316eeff1b8e0047d1a99a423ebf4b21
Arbai Fayçal <arbai.faycal@gmail.com>
2018-04-19 15:21:56 +0200

    First commit

---
+++ classifier_from_little_data.py
@@ -0,0 +1,74 @@
+# By Arbai Faycal, based on works of Francois Cholelt
+# Achieved an approx. 80% accuracy rate
+
+import h5py
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.utils import plot_model
+from keras import backend as K
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, 150, 150)
+else:
+    input_shape = (150, 150, 3)
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2), data_format="channels_first"))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_first"))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), data_format="channels_first"))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+batch_size = 16
+
+# data augmentation for training
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        'data/train',
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='binary')
+
+# data augmentation for validation
+validation_generator = test_datagen.flow_from_directory(
+        'data/validation',
+        target_size=(150, 150),
+        batch_size=batch_size,
+        class_mode='binary')
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=2000 // batch_size,
+        epochs=50,
+        verbose=2,
+        validation_data=validation_generator,
+        validation_steps=800 // batch_size)
+
+model.save_weights('model.h5')
+plot_model(model, to_file='model.png')
